16d72ca653453ac997da461858ef51c9dec91650
Josh Collins <joshua.collins820@gmail.com>
2023-11-12 21:14:17 -0800

    basement assets

---
+++ Semester Project/Assets/Characters/Player/Scripts/Stats/PlayerInventory.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [SerializeField] int inventorySlots = 5; // number of inventory slots
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
