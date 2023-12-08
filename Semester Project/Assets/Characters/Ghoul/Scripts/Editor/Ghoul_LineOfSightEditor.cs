50443a10a87f1ee2fdcfac0c4e3438ec8b097c37
Fariah Saleh <126234224+venusWaltz@users.noreply.github.com>
2023-12-07 23:45:06 -0800

    added editor for ghoul
    editor script for ghoul eyesight to help debug

---
+++ Semester Project/Assets/Characters/Ghoul/Scripts/Editor/Ghoul_LineOfSightEditor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof(Ghoul_LineOfSight))]
+public class Ghoul_LineOfSightEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        Ghoul_LineOfSight los = (Ghoul_LineOfSight)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(los.transform.position, Vector3.up, Vector3.forward, 360, los.viewRadius);
+        Vector3 viewAngleA = los.DirFromAngle(-los.viewAngle / 2, false);
+        Vector3 viewAngleB = los.DirFromAngle(los.viewAngle / 2, false);
+
+        Handles.DrawLine(los.transform.position, los.transform.position + viewAngleA * los.viewRadius);
+        Handles.DrawLine(los.transform.position, los.transform.position + viewAngleB * los.viewRadius);
+
+        if (los.IsInView())
+        {
+            Handles.color = Color.red;
+            Handles.DrawLine(los.transform.position, los.GetTarget().position);
+        }
+    }
+}
